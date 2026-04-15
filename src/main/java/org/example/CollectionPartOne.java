ab0fa92cbc1e84e4c3e5ff049590cb631b8ea0bd
Tudor Bozu <tbozu@griddynamics.com>
2026-04-15 11:58:15 +0300

    feat(study): Task 1 part 1 is done

--- src/main/java/org/example/CollectionPartOne.java
+++ src/main/java/org/example/CollectionPartOne.java
@@ -1,5 +1,38 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CollectionPartOne {
 
+    public String removeAllSymbols(){
+        SongText songText = new SongText();
+        String textBeforeChange = songText.getSongText().toString();
+
+        return textBeforeChange.replace(",","")
+                .replace("\n"," ")
+                .replace("(","")
+                .replace(")","").toLowerCase();
+    }
+
+    public String[] addStringInArrayString(){
+        String[] arrayString = removeAllSymbols().trim().split("\\s+");
+        return arrayString;
+    }
+
+    public Map countHowManyTimesAppearedInArray(){
+        addStringInArrayString();
+        Map<String,Integer> mapWithString = new HashMap<>();
+
+        for (String word : addStringInArrayString()) {
+            mapWithString.put(word, mapWithString.getOrDefault(word, 0) + 1);
+        }
+
+        for(Map.Entry coutWords: mapWithString.entrySet()){
+            System.out.println(coutWords.getKey()+ " -> " + coutWords.getValue());
+        }
+
+        return mapWithString;
+
+    }
 }
--- src/test/java/test/testCollectionPartOne.java
+++ src/test/java/test/testCollectionPartOne.java
@@ -1,4 +1,35 @@
 package test;
 
+import org.example.CollectionPartOne;
+import org.example.SongText;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class testCollectionPartOne {
+
+    CollectionPartOne collectionPartOne = new CollectionPartOne();
+
+   @Test
+    public void testIfSymbolsIsDeleted(){
+        SongText textBeforeChange = new SongText();
+       Assert.assertNotEquals(collectionPartOne.removeAllSymbols(),textBeforeChange.getSongText());
+    }
+    @Test
+    public void testIfStringToConvertArrayString(){
+       collectionPartOne.addStringInArrayString();
+       Assert.assertTrue("This it's array:",collectionPartOne.addStringInArrayString().getClass().isArray());
+    }
+
+    @Test
+    public void testHowManyTimesIsAppearedInArray(){
+       Map<String,Integer> testMapWithValues = collectionPartOne.countHowManyTimesAppearedInArray();
+           Assert.assertEquals(Integer.valueOf(1),testMapWithValues.get("who"));
+           Assert.assertEquals(Integer.valueOf(3),testMapWithValues.get("full"));
+           Assert.assertEquals(Integer.valueOf(1),testMapWithValues.get("drop"));
+           Assert.assertEquals(Integer.valueOf(11),testMapWithValues.get("a"));
+
+    }
 }
